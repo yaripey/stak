5d5cbfe12d1cada871a08057e845b520274df722
artemtrubin.letters@gmail.com <artemtrubin.letters@gmail.com>
2024-06-29 08:51:52 +0200

    Add text when you dont have cards or languages in collection yet

--- src/app/collection/cards/_components/CardsCollection.tsx
+++ src/app/collection/cards/_components/CardsCollection.tsx
@@ -6,6 +6,8 @@ import CardsTable from './CardsTable';
 import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import LanguageSelector from '@/app/_components/LanguageSelector';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 type CardsCollectionProps = {
   cards: CardType[];
@@ -25,13 +27,21 @@ export default function CardsCollection({
     doesLanguageExist ? initLanguageId : null,
   );
 
+
+  if (cards.length === 0) return (
+    <div className="pt-5 flex justify-center flex-col items-center gap-2">
+      <p className="text-lg">You have no cards yet.</p>
+      <Link href="/create/card"><Button>Create a new card</Button></Link>
+    </div>
+  )
+
   return (
     <div>
       <div className="mb-14 flex h-20 flex-wrap gap-4 p-5">
         <LanguageSelector
           languages={languages}
           languageId={languageId}
-          setLanguageId={(newId) => setLanguageId(newId)}
+          setLanguageId={(newId) => newId ? setLanguageId(parseInt(newId)) : newId}
           clearButton
         />
         <CardSearch
--- src/app/collection/languages/_components/LanguagesCollection.tsx
+++ src/app/collection/languages/_components/LanguagesCollection.tsx
@@ -2,6 +2,8 @@
 
 import { LanguageWithCardsType } from '@/server/db/schema';
 import LanguagesTable from './LanguagesTable';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 type LanguagesCollectionProps = {
   languages: LanguageWithCardsType[];
@@ -10,6 +12,12 @@ type LanguagesCollectionProps = {
 export default function LanguagesCollection({
   languages,
 }: LanguagesCollectionProps) {
+  if (languages.length === 0) return (
+    <div className="pt-5 flex justify-center flex-col items-center gap-2">
+      <p className="text-lg">You have no languages yet.</p>
+      <Link href="/create/language"><Button>Create a new language</Button></Link>
+    </div>
+  )
   return (
     <div>
       <LanguagesTable languages={languages} />
